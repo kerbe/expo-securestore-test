e9bf5ec0d70181ac924bdbc9b2722a7eabe5fa6e
Joonas Hämäläinen <47280420+kerbe@users.noreply.github.com>
2020-08-10 03:40:35 +0300

    Minimum example how SecureStore fails
    When app starts, it tries to read value from SecureStore.
    On initial load, it is empty, as is expected.
    Once app is loaded, pressing button saves value to SecureStore.
    Pressing another button loads it, and shows it is saved correctly.
    Restarting app, and saved value is read correctly, and visible.
    If expo-cli is restarted, then value is lost.

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,49 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+  const [savedValue, setSavedValue] = React.useState();
+
+  React.useEffect(() => {
+    const getValue = async () => {
+      const secureValue = await SecureStore.getItemAsync('exampleKey');
+      setSavedValue(secureValue);
+    }
+
+    getValue();
+  }, []);
+
+  const saveToSecure = async () => {
+    await SecureStore.setItemAsync('exampleKey', 'foobar');
+  };
+
+  const readFromSecure = async() => {
+    const secureValue = await SecureStore.getItemAsync('exampleKey');
+    setSavedValue(secureValue);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text>
+        This is saved value:
+        {savedValue}
+        End of Saved value.
+      </Text>
+      <Button
+        onPress={() => saveToSecure()}
+        title="Save to SecureStore"
+      />
+      <Button
+        onPress={() => readFromSecure()}
+        title="Read from SecureStore"
+      />
     </View>
   );
 }
